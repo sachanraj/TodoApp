275d9f1f744e1976d6ec4d17182578437db3c65e
Shikhar Raj Sachan <shikharrajsachan40@gmail.com>
2024-06-20 00:51:48 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import styles from "./Form.module.css"
+export default function Form({todos,setTodos}){
+    const[todo,setTodo]=useState({name:"",done:false});
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos,todo]);
+        setTodo({name:"",done:false});
+    }
+
+    return (
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>
+            <input className={styles.modernInput} placeholder="Enter todo item..."onChange={(e)=>setTodo({name:e.target.value,done:false})} value={todo.name} type="text"/>
+            <button className={styles.modernButton}type="submit">Add</button>
+        </div>
+    </form>
+)
+}
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import styles from "./Todoitem.module.css"
+
+export default function TodoItem({item,todos,setTodos}){
+    function handleDelete(item){
+        setTodos(todos.filter((todo)=>todo!==item));
+    }
+
+    function handleClick(name){
+      setTodos(todos.map((todo)=>todo.name===name?{...todo,done:!todo.done}:todo));
+    }
+
+    const className=item.done?styles.completed:"";
+
+    return <div className={styles.item}>
+        <div className={styles.itemName}>
+            <span className={className} onClick={()=>handleClick(item.name)}>{item.name}</span>
+        <span>
+            <button onClick={()=>handleDelete(item)} className={styles.deleteButton}>x</button>
+        </span>
+        </div>
+        <hr className={styles.line}></hr>
+    </div>
+}
